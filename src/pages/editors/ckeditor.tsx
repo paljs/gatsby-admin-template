e6ad8caeef682c0452793c714a6b3020c61b2ae4
Ahmed Elywa <ahmed.elywa@icloud.com>
2020-05-14 00:16:52 +0200

    update CKEditor issue

--- src/pages/editors/ckeditor.tsx
+++ src/pages/editors/ckeditor.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Card, CardBody } from 'oah-ui';
 import SEO from '../../components/SEO';
-const ClassicEditor = React.lazy(() => import('@ckeditor/ckeditor5-build-classic'));
-const CKEditor = React.lazy(() => import('@ckeditor/ckeditor5-react'));
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 export default function CKEditorPage() {
   const isSSR = typeof window === 'undefined';
@@ -12,26 +12,24 @@ export default function CKEditorPage() {
       <header>CKEditor</header>
       <CardBody>
         {!isSSR && (
-          <React.Suspense fallback={<div />}>
-            <CKEditor
-              editor={ClassicEditor}
-              data="<p>Hello from CKEditor 5!</p>"
-              onInit={(editor: any) => {
-                // You can store the "editor" and use when it is needed.
-                console.log('Editor is ready to use!', editor);
-              }}
-              onChange={(event: any, editor: { getData: () => any }) => {
-                const data = editor.getData();
-                console.log({ event, editor, data });
-              }}
-              onBlur={(event: any, editor: any) => {
-                console.log('Blur.', editor);
-              }}
-              onFocus={(event: any, editor: any) => {
-                console.log('Focus.', editor);
-              }}
-            />
-          </React.Suspense>
+          <CKEditor
+            editor={ClassicEditor}
+            data="<p>Hello from CKEditor 5!</p>"
+            onInit={(editor: any) => {
+              // You can store the "editor" and use when it is needed.
+              console.log('Editor is ready to use!', editor);
+            }}
+            onChange={(event: any, editor: { getData: () => any }) => {
+              const data = editor.getData();
+              console.log({ event, editor, data });
+            }}
+            onBlur={(event: any, editor: any) => {
+              console.log('Blur.', editor);
+            }}
+            onFocus={(event: any, editor: any) => {
+              console.log('Focus.', editor);
+            }}
+          />
         )}
       </CardBody>
     </Card>
